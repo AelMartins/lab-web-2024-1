4c7560ed75dd9e3b11159e1046dd9ae4ccfe21b4
AelMartins <oneapcwork@gmail.com>
2024-06-14 15:04:38 -0300

    Adaptado arquivo product-routes

--- api/v1/product/product-routes.js
+++ api/v1/product/product-routes.js
@@ -1,45 +1,47 @@
-const { getProducts, create, findById, deleteById} = require('./product-controller');
+const { getProducts, create, findById, deleteById } = require('./product-controller');
 const schema = require('./product-schema');
 
 const plugin = {
-    name: 'product-v1-route',
+    name: 'book-v1-route',
     version: '1',
     register: (server) => {
         server.route([
             {
                 method: "GET",
-                path: "/v1/products",
+                path: "/v1/books",
                 options: {
                     tags: ['api'],
-                    description: 'List of products',
+                    description: 'Lista todos os livros',
                     handler: getProducts,
-                    validate: schema.getProducts
+                    validate: schema.getBooks
                 }
             },
             {
                 method: "GET",
-                path: "/v1/products/{id}",
+                path: "/v1/books/{id}",
                 options: {
                     tags: ['api'],
-                    description: 'Create a product',
+                    description: 'Obtém os detalhes de um livro específico',
                     handler: findById,
                     validate: schema.getById
                 }
             },
             {
                 method: "POST",
-                path: "/v1/products",
+                path: "/v1/books",
                 options: {
                     tags: ['api'],
+                    description: 'Adiciona um novo livro',
                     handler: create,
-                    validate: schema.createProductsSchema
+                    validate: schema.createBooksSchema
                 }
             },
             {
                 method: "DELETE",
-                path: "/v1/products/{id}",
+                path: "/v1/books/{id}",
                 options: {
                     tags: ['api'],
+                    description: 'Remove um livro',
                     handler: deleteById,
                     validate: schema.deleteById
                 }
